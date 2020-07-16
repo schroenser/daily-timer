86b9bffd951c7035a89e7aa8d003be777afe9559
Sven Haberer <schroenser@gmail.com>
2020-07-16 22:51:06 +0200

    Added first version of the timer.

---
+++ app.js
@@ -0,0 +1,104 @@
+const durationPerParticipant = moment.duration(1, 'minutes');
+
+let employees = [];
+
+for (let i = employees.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = employees[i];
+    employees[i] = employees[j];
+    employees[j] = temp;
+}
+
+new Vue({
+    el: '#app',
+    data: {
+        names: employees,
+        currentIndex: 0,
+        running: false,
+        now: moment(),
+        end: moment().subtract(1, 'minutes')
+    },
+    created: function() {
+        var self = this;
+        setInterval(function() {
+            if (self.running) {
+                self.now = moment();
+            }
+        }, 10);
+    },
+    computed: {
+        hasCurrent: function() {
+            return this.currentIndex < this.names.length;
+        },
+        current: function() {
+            return this.names[this.currentIndex];
+        },
+        currentText: function() {
+            return this.running ? this.current + ' has ' + this.remainingTime.humanize({
+                ss: 0,
+                s: 60,
+                m: 60
+            }) + ' left' : this.current + ' is next';
+        },
+        pending: function() {
+            return this.names.slice(this.currentIndex + 1);
+        },
+        remainingTime: function() {
+            return moment.duration(this.end.diff(moment.min(this.end, this.now)));
+        },
+        missingTimePercentage: function() {
+            return this.running ? 100
+                - this.remainingTime.asMilliseconds()
+                * 100
+                / durationPerParticipant.asMilliseconds() : 0;
+        }
+    },
+    watch: {
+        remainingTime: function(newValue) {
+            if (this.running && newValue.asMilliseconds() <= 0) {
+                this.running = false;
+                this.currentIndex = this.currentIndex + 1;
+            }
+        }
+    },
+    methods: {
+        remove: function(index) {
+            if (this.running && index === 0) {
+                this.running = false;
+            }
+            this.names.splice(index, 1);
+        },
+        toggleTimer: function() {
+            if (this.running) {
+                this.running = false;
+            }
+            else {
+                this.now = moment();
+                this.end = moment(this.now).add(durationPerParticipant);
+                this.running = true;
+            }
+        }
+    },
+    template: `
+        <div>
+            <div
+                class="current"
+                v-if="hasCurrent">
+                <div v-on:click.prevent="remove(0)">{{ currentText }}</div>
+                <div
+                    class="current-progress-background"
+                    v-on:click.prevent="toggleTimer"></div>
+                <div
+                    class="current-progress-foreground"
+                    v-on:click.prevent="toggleTimer"
+                    v-bind:style="{ width: missingTimePercentage + '%' }"></div>
+            </div>
+            <div
+                v-for="(name, index) in pending"
+                class="pending"
+                v-on:click.prevent="remove(index + 1)">{{ name }}
+            </div>
+            <div v-if="!hasCurrent" class="fin">Fin</div>
+        </div>
+    `
+});
